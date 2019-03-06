21d52d12e5843977deb240fd7154a3907181fe37
alanbontoft <alan.bontoft@gmail.com>
2019-03-06 12:47:58 +0000

    comments added in main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import pygame as pg
 import application
 
 def main():
-    """Create an App and start the program."""
+    """Create an App and start the main loop."""
     app = application.App()
     app.main_loop()
+    
+    # clean up when main loop exits
     pg.quit()
     sys.exit()
 
